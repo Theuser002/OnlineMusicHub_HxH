a5d70b468b99619d6383f1382a6cdb1095aec88d
BysGits <quandh619@gmail.com>
2021-06-01 21:05:16 +0700

    edit account

--- app/views/layouts/account-page.php
+++ app/views/layouts/account-page.php
@@ -10,7 +10,54 @@ session_start();
 	</head>
 	<body id="account-body">
 		<?php require "navbar.php";	?>
+		<style>
 		
+			.responsive-pic{
+				position: absolute;
+				height: 20vh;
+				width: 25vh;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+			}
+			
+			.responsive-box {
+				position: absolute;
+				width: 13vh;
+			}
+			.responsive-box:after {
+				content: "";
+				display: block;
+				padding-bottom: 100%; /* The padding depends on the width, not on the height, so with a padding-bottom of 100% you will get a square */
+			}
+
+			.responsive-box img {
+				position: absolute; /* Take your picture out of the flow */
+				top: 0;
+				bottom: 0;
+				left: 0;
+				right: 0; /* Make the picture taking the size of it's parent */
+				width: 100%; /* This if for the object-fit */
+				height: 100%; /* This if for the object-fit */
+				object-fit: cover; /* Equivalent of the background-size: cover; of a background-image */
+				object-position: center;
+				border-radius: 50%;
+			}
+			
+			.content{
+				margin-top: 10vh;
+			}
+			
+			#profile-pic{
+				transform: translateY(60%);
+			}
+			
+			#profile-name{
+				color: white;
+				font-size: 3vh;
+				transform: translateY(-50%);
+			}
+		</style>
 		
 		<header id="header-account">
 			<div class="responsive-pic">
