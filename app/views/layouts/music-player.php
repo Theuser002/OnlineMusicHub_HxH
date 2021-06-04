0a26f8cae39b60f2510cd0d4ed1e925ccae23595
I001user <hung.nt176773@sis.hust.edu.vn>
2021-06-04 12:59:57 +0700

    structures functions in songs-display-controller

--- app/views/layouts/music-player.php
+++ app/views/layouts/music-player.php
@@ -36,42 +36,32 @@ $song = $songPagList[$songIndex];
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 <link rel="stylesheet" type="text/css" href="css/music-player.css">
 </head>
-<body id="music-player-body">
-<?php include('navbar.php') ?>
-<header class="song-header">
-    <?php 
-       echo '<h1>All Songs</h1>';
-    ?>
-</header>
-<div class="content">
-    <div class="music-container" id="music-container">
+<body>
+<h1> HxH Song Player </h1>
+<div class="music-container" id="music-container">
     <div class="music-info">
-        <h4 id="title">
-            <?php
-            echo $song->getSongTitle();
-            ?>
-        </h4>
-        <h6 id="views">
-            <?php
-            echo $song->getViews() . ' views';
-            ?>
-        </h6>
+        <h4 id="title">StarSky</h4>
+        
         <div class="progress-container" id="progress-container">
-            <div class="progress" id="progress"></div>
+            <div class="progress" id="progress">
+            
+            </div>
         </div>
     </div>
-    <audio src="<?php echo $song->getAudioLink(); ?>" id="audio"></audio>
-    <div class="img-container"><img src="<?php echo $song->getSongImageLink(); ?>" alt="music-cover" id="cover"></div>
+    
+    <audio src="../songs/Victory.mp3" id="audio"></audio>
+    
+    <div class="img-container">
+        <img src="images/ocean.jpg" alt="music-cover" id="cover"> 
+    </div>
+    
     <div class="navigation">
-<<<<<<< HEAD
         <a id="prev" class="action-btn" href="<?php
                                                 echo 'music-player.php?tab='.$tab.'&page='.$page.'&entriesPerPage='.$entriesPerPage.'&index='.($songIndex-1);
                                               ?>">
-=======
-        <a id="prev" class="action-btn" href="music-player.php?songID=<?php echo($prevID); ?>&prevID=<?php echo($prevID); ?>&nextID=<?php echo($nextID); ?>">
->>>>>>> 8a18e47 (rewrite some functions)
             <i class="fas fa-backward"></i>
         </a>
         
@@ -79,29 +69,24 @@ $song = $songPagList[$songIndex];
             <i class="fas fa-play"></i>
         </a>
         
-<<<<<<< HEAD
         <a id="next" class="action-btn" href="<?php
                                                 echo 'music-player.php?tab='.$tab.'&page='.$page.'&entriesPerPage='.$entriesPerPage.'&index='.($songIndex+1);
                                               ?>">
-=======
-        <a id="next" class="action-btn" href="music-player.php?songID=<?php echo($nextID); ?>&prevID=<?php echo($prevID); ?>&nextID=<?php echo($nextID); ?>">
->>>>>>> 8a18e47 (rewrite some functions)
             <i class="fas fa-forward"></i>
         </a>
     </div>
     <?php
         include 'music-player.script.php';
-<<<<<<< HEAD
 //        echo $songIndex+1;
 //        echo $song->getSongID();
-=======
-        echo $songID;
-        echo $prevID;
-        echo $nextID;
->>>>>>> 8a18e47 (rewrite some functions)
     ?>
         
         
 </div>
+        <button id="prev" class="action-btn"> <i class="fas fa-backward"></i> </button>
+        <button id="play" class="action-btn action-btn-big"> <i class="fas fa-play"></i> </button>
+        <button id="next" class="action-btn"> <i class="fas fa-forward"></i> </button>
+    </div>
+    <script src="js/music-player.js"></script> 
 </div>
-<?php include 'footer.php' ?>
+</body>
