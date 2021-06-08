747db7575014f8998f95e2c3e94930cca3bc283c
Thanh Son <sonbeohaudau@gmail.com>
2021-06-08 10:06:33 +0700

    update single-singer-pagebar

--- app/views/layouts/single-singer-pagebar.php
+++ app/views/layouts/single-singer-pagebar.php
@@ -9,6 +9,16 @@
             echo "<a href='single-singer-page.php?page=" . ( $page - 1 ) . "&singerID=".$singerID."&tab=" . $tab . "'> < </a>";
         }
 
+        for ( $i = $page - $displayRange; $i <= $page + $displayRange; $i++ ) {
+            if ( $i > 0 && $i <= $number_of_page ) {
+                if ( $i == $page ) {
+                    echo "<a class = \"chosen\" href='single-singer-page.php?page=" . $i . "&singerID=".$singerID."&tab=" . $tab . "'> " . $i . " </a>";
+                } else {
+                    echo "<a href='single-singer-page.php?page=" . $i . "&singerID=".$singerID."&tab=" . $tab . "'> " . $i . " </a>";
+                }
+
+            }
+        }
 
 
         if ( $page < $number_of_page ) {
