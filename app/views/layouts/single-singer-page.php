7e760548dec347816745f0d19c34fc1511dc4bc1
Thanh Son <sonbeohaudau@gmail.com>
2021-06-08 10:04:49 +0700

    update single singer page

--- app/views/layouts/single-singer-page.php
+++ app/views/layouts/single-singer-page.php
@@ -29,4 +29,33 @@ session_start();
     $singerName = $singer->getSingerName();
     $singerBackground = $singer->getBackground();
 
- 
+    
+    
+    if($tab == 1){
+        //Songs
+        $songlist = $c->getSingerSong($_GET['singerID']);
+        $listlen = count($songlist);
+        //define total number of results you want per page
+        $result_per_page = 5;
+        $SongPagList = $c->getPaginationSong($page, $result_per_page, $singerID);
+    }else {
+        //MVs
+        $mvlist = $c->getSingerMV($_GET['singerID']);
+        $listlen = count($mvlist);
+        //define total number of results you want per page
+        $result_per_page = 8;
+        $MVPagList = $c->getPaginationMV($page, $result_per_page, $singerID);
+    }
+    //determine the total number of pages available  
+    $number_of_page = ceil($listlen/$result_per_page);
+?>
+
+<!DOCTYPE HTML>
+<head>
+    <title><?php echo $singerName ?></title>
+	<link rel="icon" type="image/png" sizes="32x32" href="images/logo.png">
+</head>
+<body id="single-singer-body">
+    <?php
+        require "navbar.php";
+    ?>
